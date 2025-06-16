289ec3429e2d683cd0b0079d2fb13ac6f20e0f57
Wijaya-Putra <igedeputrawijaya@gmail.com>
2025-06-16 10:07:34 +0700

    Analytics and Speed Insight Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const openSans = Open_Sans({
